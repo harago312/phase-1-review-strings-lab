3ce3624ee81baf001fdc004eaa0d55d1144ebd6a
harago312 <ayisse312@gmail.com>
2022-11-24 21:31:21 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+console.log(currentUser)
+/*const welcomeMessage = 'Welcome to Flatbook, ';*/
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(welcomeMessage)
+console.log(excitedWelcomeMessage)
+
+/*const cUser = 'G'*
+const shortGreeting = '';*/
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting)
